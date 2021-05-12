c05f758c201908b51f7ff084468670aefe154b10
Alice <gust4f3rn@gmail.com>
2021-05-12 14:36:49 -0300

    Help comments added

--- app.py
+++ app.py
@@ -1,38 +1,6 @@
 #!/usr/local/bin/python3
 import numpy as np
 import cv2 
-# from PIL import Image
-
-# # Open image and make RGB and HSV versions
-# RGBim = Image.open("./Syngenta.bmp").convert('RGB')
-# HSVim = RGBim.convert('HSV')
-
-# # Make numpy versions
-# RGBna = np.array(RGBim)
-# HSVna = np.array(HSVim)
-
-# # Extract Hue
-# H = HSVna[:,:,0]
-
-# # print(H, RGBna)
-
-# # Find all green pixels, i.e. where 100 < Hue < 140
-# lo,hi = 100,140
-# # Rescale to 0-255, rather than 0-360 because we are using uint8
-# lo = int((lo * 255) / 360)
-# hi = int((hi * 255) / 360)
-# green = np.where((H>lo) & (H<hi))
-
-# # Make all green pixels black in original image
-# RGBna[green] = [0,0,0]
-
-# count = green[0].size
-# print("Pixels matched: {}".format(count))
-
-# # Image.fromarray(RGBna).save('result.png')
-
-import cv2
-import numpy as np
 
 img = cv2.imread("Syngenta.bmp")
 
@@ -44,6 +12,10 @@ upper_range = np.array([70, 255,255])
 mask = cv2.inRange(hsv, lower_range, upper_range)
 
 greenAmount = cv2.countNonZero(mask)
+
+
+# Quando rodar o código, ele abrira a imagem original e a imagem processada, 
+# para fechar as imagens e ver a quantidade de pixels verdes aperte 0 no teclado
 
 cv2.imshow("Image", img)
 cv2.imshow("Mask", mask)
